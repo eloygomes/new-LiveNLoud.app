a63ae7de166044b2be5bff1c421442d07191ec51
Eloy Gomes <eloy.gomes@icloud.com>
2024-08-08 05:42:54 -0300

    0.3.0.0
    .

---
+++ Front/src/Pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import DashboardList from "./DashboardList";
+
+function Dashboard() {
+  return (
+    <div className=" flex justify-center h-screen">
+      <div className="container mx-auto">
+        <DashboardList />
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
---
+++ Front/src/Pages/EditSong/EditSong.jsx
@@ -0,0 +1,11 @@
+function EditSong() {
+  return (
+    <div className=" flex justify-center h-screen">
+      <div className="container mx-auto">
+        <h1>Edit SONG</h1>
+      </div>
+    </div>
+  );
+}
+
+export default EditSong;
