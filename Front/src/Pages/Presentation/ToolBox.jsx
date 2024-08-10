909e0dd23e6b3ecc2f166bdc27e261b30431787c
Eloy Gomes <eloy.gomes@icloud.com>
2024-08-10 09:02:13 -0300

    0.7.0.0
    - Fixed problem with presentation page(the fix was made on nginx file on server)
    
    - Working on toolbar on presentation too box

--- Front/src/Pages/Presentation/ToolBox.jsx
+++ Front/src/Pages/Presentation/ToolBox.jsx
@@ -1,27 +1,106 @@
+/* eslint-disable react/prop-types */
 import FloatingBtns from "./FloatingBtns";
 import FloatingBtnsAutoScroll from "./FloatingBtnsAutoScroll";
 
-// eslint-disable-next-line react/prop-types
-function ToolBox({ toolBoxBtnStatus }) {
+function ToolBox({
+  toolBoxBtnStatus,
+  setToolBoxBtnStatus,
+  toolBoxBtnStatusChange,
+}) {
   return (
     <>
       <div className={`${toolBoxBtnStatus ? "flex" : "hidden"}`}>
-        <div className=" w-40 h-[88vh] flex flex-col justify-between neuphormism-b-se fixed right-16 bottom-3 p-4">
-          <div className="border-b-2 border-gray-300 w-full ">
-            <div className="flex flex-row text-2xl font-semibold">MENU</div>
-          </div>
-          <div className="border-b-2 border-gray-300 w-full ">
-            <div className="flex flex-row text-2xl font-semibold">PARTS</div>
-            <ul>
-              <li>intro</li>
-              <li>verse</li>
-              <li>chorus</li>
-              <li>bridge</li>
-              <li>chorus</li>
-            </ul>
+        <div className=" w-40 h-[90vh] flex flex-col justify-between neuphormism-b-se fixed right-16 bottom-3 p-4">
+          <div>
+            <div className="border-b-2 border-gray-300 w-full flex flex-row justify-between  py-2">
+              <div className="flex flex-row text-sm font-bold">
+                FAST NAVIGATION
+              </div>
+              <div
+                className="flex flex-row text-2xl font-semibold hover:font-black"
+                onClick={() =>
+                  toolBoxBtnStatusChange(toolBoxBtnStatus, setToolBoxBtnStatus)
+                }
+              >
+                X
+              </div>
+            </div>
+            <div className="border-b-2 border-gray-300 w-full ">
+              <div className="flex flex-row text-md font-semibold py-2">
+                PARTS
+              </div>
+              <ul>
+                <li className="hover:font-semibold">
+                  <a href="#">intro</a>
+                </li>
+                <li className="hover:font-semibold">
+                  <a href="#">verse</a>
+                </li>
+                <li className="hover:font-semibold">
+                  <a href="#">chorus</a>
+                </li>
+                <li className="hover:font-semibold">
+                  <a href="#">bridge</a>
+                </li>
+                <li className="hover:font-semibold">
+                  <a href="#">chorus</a>
+                </li>
+              </ul>
+            </div>
+            <div className="border-b-2 border-gray-300 w-full mb-5">
+              <div className="flex flex-row text-md font-semibold py-2">
+                HIGHLIGHT
+              </div>
+              <ul>
+                <li className="hover:font-semibold">
+                  <button
+                    type="button"
+                    className="neuphormism-b-se w-full my-2 "
+                  >
+                    notes
+                  </button>
+                </li>
+                <li className="hover:font-semibold">
+                  <button
+                    type="button"
+                    className="neuphormism-b-se w-full my-2"
+                  >
+                    lyrics
+                  </button>
+                </li>
+              </ul>
+            </div>
+            <div className="border-b-2 border-gray-300 w-full mb-5">
+              <div className="flex flex-row text-md font-semibold py-2">
+                TOOLS
+              </div>
+              <ul>
+                <li className="hover:font-semibold">
+                  <button
+                    type="button"
+                    className="neuphormism-b-se w-full my-2 "
+                  >
+                    tuner
+                  </button>
+                </li>
+                <li className="hover:font-semibold">
+                  <button
+                    type="button"
+                    className="neuphormism-b-se w-full my-2"
+                  >
+                    metronome
+                  </button>
+                </li>
+              </ul>
+            </div>
           </div>
 
-          <div className="flex flex-row">
+          <div className="flex flex-row h-44">
+            <div className="border-b-2 border-gray-300 w-full mb-36">
+              <div className="flex flex-row text-md font-semibold py-2">
+                SCROLLING
+              </div>
+            </div>
             <FloatingBtns />
             <FloatingBtnsAutoScroll />
           </div>
