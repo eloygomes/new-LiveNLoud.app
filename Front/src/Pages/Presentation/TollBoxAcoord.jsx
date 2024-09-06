c6d9e7f213bf4eeecbf72182cd62821ea528dfbe
Eloy Gomes <eloy.gomes@icloud.com>
2024-09-06 17:26:40 -0300

    0.19.10.0
    => Presentation page
    - logic on toolbox links to others instruments done
    
    => New song page
    - Fix the new song

--- Front/src/Pages/Presentation/TollBoxAcoord.jsx
+++ Front/src/Pages/Presentation/TollBoxAcoord.jsx
@@ -1,223 +1,4 @@
-// /* eslint-disable react/jsx-key */
-// import Accordion from "@mui/material/Accordion";
-
-// import AccordionSummary from "@mui/material/AccordionSummary";
-// import AccordionDetails from "@mui/material/AccordionDetails";
-// import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-
-// import FloatingBtns from "./FloatingBtns";
-// import FloatingBtnsAutoScroll from "./FloatingBtnsAutoScroll";
-// import ToolBoxYT from "./ToolBoxYT";
-
-// export default function TollBoxAcoord({
-//   embedLinks,
-//   setLinktoplay,
-//   setVideoModalStatus,
-// }) {
-//   // console.log(embedLinks);
-//   const handlePlayClick = (url) => {
-//     console.log("setLinktoplay", url);
-//     setLinktoplay(url); // Atualiza o link do vídeo para ser reproduzido
-//     setVideoModalStatus(true);
-//   };
-//   return (
-//     <div>
-//       {/* <ToolBoxYT embedLinks={embedLinks} /> */}
-//       <Accordion>
-//         <AccordionSummary
-//           expandIcon={<ExpandMoreIcon />}
-//           aria-controls="panel1-content"
-//           id="panel1-header"
-//           className="neuphormism-b text-sm font-semibold py-1 rounded-lg"
-//         >
-//           Instruments
-//         </AccordionSummary>
-//         <AccordionDetails className="neuphormism-b text-sm font-semibold">
-//           <ul className="mb-5">
-//             <li className="hover:font-semibold flex flex-row">
-//               <button
-//                 type="button"
-//                 className="neuphormism-b-se w-1/2 p-2 m-2 text-sm"
-//               >
-//                 G1
-//               </button>
-//               <button
-//                 type="button"
-//                 className="neuphormism-b-se w-1/2 p-2 m-2 text-sm"
-//               >
-//                 G2
-//               </button>
-//             </li>
-//             <li className="hover:font-semibold flex flex-row">
-//               <button
-//                 type="button"
-//                 className="neuphormism-b-se w-1/2 p-2 m-2 text-sm"
-//               >
-//                 B
-//               </button>
-//               <button
-//                 type="button"
-//                 className="neuphormism-b-se w-1/2 p-2 m-2 text-sm"
-//               >
-//                 K
-//               </button>
-//             </li>
-//             <li className="hover:font-semibold flex flex-row">
-//               <button
-//                 type="button"
-//                 className="neuphormism-b-se w-1/2 p-2 m-2 text-sm"
-//               >
-//                 D
-//               </button>
-//               <button
-//                 type="button"
-//                 className="neuphormism-b-se w-1/2 p-2 m-2 text-sm"
-//               >
-//                 V
-//               </button>
-//             </li>
-//           </ul>
-//         </AccordionDetails>
-//       </Accordion>
-//       {/* <Accordion defaultExpanded className="my-2"> */}
-//       <Accordion className="my-2">
-//         <AccordionSummary
-//           expandIcon={<ExpandMoreIcon />}
-//           aria-controls="panel2-content"
-//           id="panel2-header"
-//           className="neuphormism-b text-sm font-semibold py-1 rounded-lg"
-//         >
-//           Embed link
-//         </AccordionSummary>
-//         <AccordionDetails className="neuphormism-b text-sm font-semibold">
-//           <ul className="mb-5">
-//             {embedLinks.map((link, index) => (
-//               <li key={index} className="hover:font-semibold flex flex-row">
-//                 <button
-//                   type="button"
-//                   className="neuphormism-b-se w-full p-2 m-2 text-sm"
-//                   onClick={() => handlePlayClick(link)}
-//                 >
-//                   watch {index + 1}
-//                 </button>
-//               </li>
-//             ))}
-//           </ul>
-//         </AccordionDetails>
-//       </Accordion>
-//       <Accordion className="my-2">
-//         <AccordionSummary
-//           expandIcon={<ExpandMoreIcon />}
-//           aria-controls="panel2-content"
-//           id="panel2-header"
-//           className="neuphormism-b text-sm font-semibold py-1 rounded-lg"
-//         >
-//           Navegation
-//         </AccordionSummary>
-//         <AccordionDetails className="neuphormism-b text-sm font-semibold">
-//           <ul className="mb-5">
-//             <li className="hover:bg-gray-300 neuphormism-b-se  p-2 my-3 font-normal">
-//               <a href="#">intro</a>
-//             </li>
-//             <li className="hover:bg-gray-300 neuphormism-b-se  p-2 my-3 font-normal">
-//               <a href="#">verse</a>
-//             </li>
-//             <li className="hover:bg-gray-300 neuphormism-b-se  p-2 my-3 font-normal">
-//               <a href="#">chorus</a>
-//             </li>
-//             <li className="hover:bg-gray-300 neuphormism-b-se  p-2 my-3 font-normal">
-//               <a href="#">bridge</a>
-//             </li>
-//             <li className="hover:bg-gray-300 neuphormism-b-se  p-2 my-3 font-normal">
-//               <a href="#">chorus</a>
-//             </li>
-//           </ul>
-//         </AccordionDetails>
-//       </Accordion>
-
-//       <Accordion className="my-2">
-//         <AccordionSummary
-//           expandIcon={<ExpandMoreIcon />}
-//           aria-controls="panel3-content"
-//           id="panel3-header"
-//           className="neuphormism-b text-sm font-semibold py-1 rounded-lg"
-//         >
-//           Highlight
-//         </AccordionSummary>
-//         <AccordionDetails className="neuphormism-b text-sm font-semibold">
-//           <ul className="m-2">
-//             <li className="hover:font-semibold">
-//               <button type="button" className="neuphormism-b-se w-full my-2 ">
-//                 notes
-//               </button>
-//             </li>
-//             <li className="hover:font-semibold">
-//               <button type="button" className="neuphormism-b-se w-full my-2">
-//                 lyrics
-//               </button>
-//             </li>
-//           </ul>
-//         </AccordionDetails>
-//       </Accordion>
-//       <Accordion className="my-2">
-//         <AccordionSummary
-//           expandIcon={<ExpandMoreIcon />}
-//           aria-controls="panel3-content"
-//           id="panel3-header"
-//           className="neuphormism-b text-sm font-semibold py-1 rounded-lg"
-//         >
-//           Tools
-//         </AccordionSummary>
-//         <AccordionDetails className="neuphormism-b text-sm font-semibold">
-//           <ul className="my-5">
-//             <li className="hover:font-semibold">
-//               <div className="p-10  rounded-md mb-2 neuphormism-b"></div>
-//             </li>
-//             <li className="hover:font-semibold">
-//               <button type="button" className="neuphormism-b-se w-full my-2 ">
-//                 tuner
-//               </button>
-//             </li>
-//             <li className="hover:font-semibold">
-//               <button type="button" className="neuphormism-b-se w-full my-2">
-//                 metronome
-//               </button>
-//             </li>
-//             <li className="hover:font-semibold">
-//               <button type="button" className="neuphormism-b-se w-full my-2">
-//                 chord library
-//               </button>
-//             </li>
-//           </ul>
-//         </AccordionDetails>
-//       </Accordion>
-//       <Accordion className="my-2">
-//         <AccordionSummary
-//           expandIcon={<ExpandMoreIcon />}
-//           aria-controls="panel3-content"
-//           id="panel3-header"
-//           className="neuphormism-b text-sm font-semibold py-1 rounded-lg"
-//         >
-//           Scrolling
-//         </AccordionSummary>
-//         <AccordionDetails className="neuphormism-b text-sm font-semibold">
-//           <div className="my-5">
-//             <div className="flex flex-row h-44">
-//               <div className="border-b-2 border-gray-300 w-full mb-36">
-//                 <div className="flex flex-row text-sm font-semibold py-2">
-//                   SCROLLING
-//                 </div>
-//               </div>
-//               <FloatingBtns />
-//               <FloatingBtnsAutoScroll />
-//             </div>
-//           </div>
-//         </AccordionDetails>
-//       </Accordion>
-//     </div>
-//   );
-// }
-
+/* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-key */
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
@@ -226,20 +7,34 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 import FloatingBtns from "./FloatingBtns";
 import FloatingBtnsAutoScroll from "./FloatingBtnsAutoScroll";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function TollBoxAcoord({
   embedLinks,
   setLinktoplay,
   setVideoModalStatus,
+  songFromURL,
+  artistFromURL,
+  instrumentSelected,
 }) {
+  const [expanded, setExpanded] = useState(false); // Estado para controlar o acordeão aberto
+
   const handlePlayClick = (url) => {
     setLinktoplay(url); // Atualiza o link do vídeo para ser reproduzido
     setVideoModalStatus(true); // Abre o modal do player
   };
 
+  const handleAccordionChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false); // Abre apenas o acordeão clicado, fecha os outros
+  };
+
   return (
     <div>
-      <Accordion>
+      <Accordion
+        expanded={expanded === "panel1"}
+        onChange={handleAccordionChange("panel1")}
+      >
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1-content"
@@ -253,50 +48,128 @@ export default function TollBoxAcoord({
             <li className="hover:font-semibold flex flex-row">
               <button
                 type="button"
-                className="neuphormism-b-se w-1/2 p-2 m-2 text-sm"
+                className={`w-1/2 p-2 m-2 text-sm ${
+                  instrumentSelected === "guitar01"
+                    ? "neuphormism-b-btn"
+                    : "neuphormism-b-btn-desactivated"
+                }`}
               >
-                G1
+                {instrumentSelected === "guitar01" ? (
+                  <Link
+                    to={`/presentation/${artistFromURL}/${songFromURL}/guitar01}`}
+                  >
+                    G1
+                  </Link>
+                ) : (
+                  "G1"
+                )}
               </button>
+
               <button
                 type="button"
-                className="neuphormism-b-se w-1/2 p-2 m-2 text-sm"
+                className={`w-1/2 p-2 m-2 text-sm ${
+                  instrumentSelected === "guitar02"
+                    ? "neuphormism-b-btn"
+                    : "neuphormism-b-btn-desactivated"
+                }`}
               >
-                G2
+                {instrumentSelected === "guitar02" ? (
+                  <Link
+                    to={`/presentation/${artistFromURL}/${songFromURL}/guitar02}`}
+                  >
+                    G2
+                  </Link>
+                ) : (
+                  "G2"
+                )}
               </button>
             </li>
             <li className="hover:font-semibold flex flex-row">
               <button
                 type="button"
-                className="neuphormism-b-se w-1/2 p-2 m-2 text-sm"
+                className={`w-1/2 p-2 m-2 text-sm ${
+                  instrumentSelected === "bass"
+                    ? "neuphormism-b-btn"
+                    : "neuphormism-b-btn-desactivated"
+                }`}
               >
-                B
+                {instrumentSelected === "bass" ? (
+                  <Link
+                    to={`/presentation/${artistFromURL}/${songFromURL}/bass}`}
+                  >
+                    B
+                  </Link>
+                ) : (
+                  "B"
+                )}
               </button>
+
               <button
                 type="button"
-                className="neuphormism-b-se w-1/2 p-2 m-2 text-sm"
+                className={`w-1/2 p-2 m-2 text-sm ${
+                  instrumentSelected === "keys"
+                    ? "neuphormism-b-btn"
+                    : "neuphormism-b-btn-desactivated"
+                }`}
               >
-                K
+                {instrumentSelected === "keys" ? (
+                  <Link
+                    to={`/presentation/${artistFromURL}/${songFromURL}/keys}`}
+                  >
+                    K
+                  </Link>
+                ) : (
+                  "K"
+                )}
               </button>
             </li>
             <li className="hover:font-semibold flex flex-row">
               <button
                 type="button"
-                className="neuphormism-b-se w-1/2 p-2 m-2 text-sm"
+                className={`w-1/2 p-2 m-2 text-sm ${
+                  instrumentSelected === "drums"
+                    ? "neuphormism-b-btn"
+                    : "neuphormism-b-btn-desactivated"
+                }`}
               >
-                D
+                {instrumentSelected === "drums" ? (
+                  <Link
+                    to={`/presentation/${artistFromURL}/${songFromURL}/drums}`}
+                  >
+                    D
+                  </Link>
+                ) : (
+                  "D"
+                )}
               </button>
               <button
                 type="button"
-                className="neuphormism-b-se w-1/2 p-2 m-2 text-sm"
+                className={`w-1/2 p-2 m-2 text-sm ${
+                  instrumentSelected === "voice"
+                    ? "neuphormism-b-btn"
+                    : "neuphormism-b-btn-desactivated"
+                }`}
               >
-                V
+                {instrumentSelected === "voice" ? (
+                  <Link
+                    to={`/presentation/${artistFromURL}/${songFromURL}/voice}`}
+                  >
+                    V
+                  </Link>
+                ) : (
+                  "V"
+                )}
               </button>
             </li>
           </ul>
         </AccordionDetails>
       </Accordion>
 
-      <Accordion className="my-2">
+      <Accordion
+        expanded={expanded === "panel2"}
+        onChange={handleAccordionChange("panel2")}
+        className="my-2"
+      >
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel2-content"
@@ -322,7 +195,11 @@ export default function TollBoxAcoord({
         </AccordionDetails>
       </Accordion>
 
-      <Accordion className="my-2">
+      <Accordion
+        expanded={expanded === "panel3"}
+        onChange={handleAccordionChange("panel3")}
+        className="my-2"
+      >
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel3-content"
@@ -352,7 +229,11 @@ export default function TollBoxAcoord({
         </AccordionDetails>
       </Accordion>
 
-      <Accordion className="my-2">
+      <Accordion
+        expanded={expanded === "panel4"}
+        onChange={handleAccordionChange("panel4")}
+        className="my-2"
+      >
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel4-content"
@@ -377,7 +258,11 @@ export default function TollBoxAcoord({
         </AccordionDetails>
       </Accordion>
 
-      <Accordion className="my-2">
+      <Accordion
+        expanded={expanded === "panel5"}
+        onChange={handleAccordionChange("panel5")}
+        className="my-2"
+      >
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel5-content"
@@ -410,7 +295,11 @@ export default function TollBoxAcoord({
         </AccordionDetails>
       </Accordion>
 
-      <Accordion className="my-2">
+      <Accordion
+        expanded={expanded === "panel6"}
+        onChange={handleAccordionChange("panel6")}
+        className="my-2"
+      >
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel6-content"
--- Front/src/Pages/Presentation/ToolBox.jsx
+++ Front/src/Pages/Presentation/ToolBox.jsx
@@ -1,70 +1,3 @@
-// /* eslint-disable react/prop-types */
-
-// import { useState } from "react";
-// import DraggableComponent from "./DraggableComponent";
-// import TollBoxAcoord from "./TollBoxAcoord";
-// import ToolBoxYT from "./ToolBoxYT";
-// import VideoDragComp from "./videoDragComp";
-
-// function ToolBox({
-//   toolBoxBtnStatus,
-//   setToolBoxBtnStatus,
-//   toolBoxBtnStatusChange,
-//   embedLinks,
-// }) {
-//   const [linktoplay, setLinktoplay] = useState("");
-//   const [videoModalStatus, setVideoModalStatus] = useState(false);
-//   console.log("linktoplay", linktoplay);
-//   console.log("videoModalStatus", videoModalStatus);
-//   return (
-//     <>
-//       {linktoplay ? (
-//         <div className={`${videoModalStatus ? "fixed" : "hidden"}`}>
-//           <VideoDragComp toolBoxBtnStatus={toolBoxBtnStatus}>
-//             <ToolBoxYT
-//               embedLinks={embedLinks}
-//               linktoplay={linktoplay}
-//               setVideoModalStatus={setVideoModalStatus}
-//             />
-//           </VideoDragComp>
-//         </div>
-//       ) : null}
-//       <DraggableComponent toolBoxBtnStatus={toolBoxBtnStatus}>
-//         <div className={`${toolBoxBtnStatus ? "flex" : "hidden"}`}>
-//           <div className=" w-40  flex flex-col justify-between neuphormism-b fixed right-16 bottom-3 p-2">
-//             <div>
-//               <div className="border-b-2 border-gray-300 w-full flex flex-row justify-between  py-2">
-//                 <h1 className="flex flex-row text-sm font-bold">ToolBox</h1>
-//                 <div
-//                   className="flex flex-row text-2xl font-semibold hover:font-black"
-//                   onClick={() =>
-//                     toolBoxBtnStatusChange(
-//                       toolBoxBtnStatus,
-//                       setToolBoxBtnStatus
-//                     )
-//                   }
-//                 >
-//                   X
-//                 </div>
-//               </div>
-//               <TollBoxAcoord
-//                 embedLinks={embedLinks}
-//                 setLinktoplay={setLinktoplay}
-//                 setVideoModalStatus={setVideoModalStatus}
-//               />
-//             </div>
-//             <h1 className="flex flex-row text-[6pt] font-bold items-center justify-center mx-auto w-full bg-gray-500 text-white">
-//               Click and hold to drag
-//             </h1>
-//           </div>
-//         </div>
-//       </DraggableComponent>
-//     </>
-//   );
-// }
-
-// export default ToolBox;
-
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 import DraggableComponent from "./DraggableComponent";
@@ -77,6 +10,9 @@ function ToolBox({
   setToolBoxBtnStatus,
   toolBoxBtnStatusChange,
   embedLinks,
+  songFromURL,
+  artistFromURL,
+  instrumentSelected,
 }) {
   const [linktoplay, setLinktoplay] = useState("");
   const [videoModalStatus, setVideoModalStatus] = useState(false);
@@ -119,6 +55,9 @@ function ToolBox({
                 embedLinks={embedLinks}
                 setLinktoplay={setLinktoplay} // Define o linktoplay ao clicar em um vídeo
                 setVideoModalStatus={setVideoModalStatus} // Abre o modal do player ao clicar em um vídeo
+                songFromURL={songFromURL}
+                artistFromURL={artistFromURL}
+                instrumentSelected={instrumentSelected}
               />
             </div>
             <h1 className="flex flex-row text-[6pt] font-bold items-center justify-center mx-auto w-full bg-gray-500 text-white">
