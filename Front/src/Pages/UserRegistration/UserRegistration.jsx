5c71930554e9628dff5a1a794d57b4a304cd3a67
Eloy Gomes <eloy.gomes@icloud.com>
2024-10-18 19:22:00 -0300

    0.26.3.0
    - fixes

--- Front/src/Pages/UserRegistration/UserRegistration.jsx
+++ Front/src/Pages/UserRegistration/UserRegistration.jsx
@@ -3,21 +3,23 @@ import musician from "../../assets/musician.jpg";
 
 function UserRegistration() {
   return (
-    <div className=" flex justify-center h-screen pt-20">
+    <div className=" flex justify-center h-screen pt-5">
       <div className="container mx-auto">
-        <div className="h-screen w-11/12 2xl:w-9/12 mx-auto ">
+        <div className="h-screen w-11/12 2xl:w-9/12 mx-auto flex flex-col">
           <div className="flex flex-row my-5 neuphormism-b p-5">
             <h1 className="text-4xl font-bold">User Registration</h1>
             <h4 className="ml-auto mt-auto text-sm">
               &ldquo;Cara, crachá time&ldquo;
             </h4>
           </div>
-          <div className="flex flex-row my-5 neuphormism-b p-5">
-            <div className="w-2/3 bg-green-300 flex items-center justify-center">
-              <img src={musician} className="h-full object-cover" alt="" />
+          <div className="flex flex-row my-5 neuphormism-b p-5 h-[75vh]">
+            <div className="w-1/2 flex items-center justify-center">
+              <img src={musician} className=" h-[70vh] object-cover" alt="" />
             </div>
-            <div className="w-1/3 flex flex-col justify-between mb-20 ">
-              <UserRegistrationForm />
+            <div className="w-1/2 flex flex-col justify-between  ">
+              <div className="mx-10">
+                <UserRegistrationForm />
+              </div>
             </div>
           </div>
         </div>
