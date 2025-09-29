1e28e24ce77f6978b727f8f8025512c2669c1b9a
Eloy Gomes <eloy.gomes@icloud.com>
2025-09-29 19:28:44 -0300

    0.40.2.0
    - UserData layout fixes

--- Front/src/Pages/Dashboard/SoftVersion.jsx
+++ Front/src/Pages/Dashboard/SoftVersion.jsx
@@ -2,7 +2,7 @@ export default function SoftVersion() {
   return (
     <>
       <div className="fixed top-5 right-32 p-2 z-30   ">
-        <p className=" text-[10px] ">VER.:0.40.1.0</p>
+        <p className=" text-[10px] ">VER.:0.40.2.0</p>
       </div>
     </>
   );
