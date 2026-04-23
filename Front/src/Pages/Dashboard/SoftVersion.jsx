cce2a426d5966c4c0ce00fc91c98eb21632327d3
Eloy Gomes <eloy.gomes@icloud.com>
2026-04-23 15:56:04 -0300

    0.70.1.0

--- Front/src/Pages/Dashboard/SoftVersion.jsx
+++ Front/src/Pages/Dashboard/SoftVersion.jsx
@@ -2,7 +2,7 @@ export default function SoftVersion() {
   return (
     <>
       <div className="   ">
-        <p className=" text-[10px] ">VER.:0.67.0.0</p>
+        <p className=" text-[10px] ">VER.:0.70.0.0</p>
       </div>
     </>
   );
